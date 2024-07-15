3aca3b918a3ff856cd716f82278ef7f1f44fb557
GreenGraped <121028592+GreenGraped@users.noreply.github.com>
2024-07-15 23:38:09 +0900

    fixed
    phew

--- Assets/Code/Player/Player.cs
+++ Assets/Code/Player/Player.cs
@@ -10,12 +10,8 @@ public class Player : MonoBehaviour
     private bool onGround;
     private Vector2 playerDir;
     private int currentJumpCount;
-    private bool isGrappling = false;
-    private Vector2 grapplePoint;
-    private Vector2 grappleStartPoint;
     private LineRenderer lineRenderer;
 
-    [SerializeField] private float grapplingSpeed;
     [SerializeField] private int maxJumpCount;
     [SerializeField] private float speed;
     [SerializeField] private float maxSpeed;
@@ -52,19 +48,7 @@ public class Player : MonoBehaviour
         {
             currentJumpCount = 0;
         }
-
-        if (!isGrappling)
-        {
-            HandleMovement(isMoving);
-        }
-    }
-
-    void FixedUpdate()
-    {
-        if (isGrappling)
-        {
-            HandleGrappling();
-        }
+        HandleMovement(isMoving);
     }
 
     private void HandleMovement(bool isMoving)
@@ -84,26 +68,6 @@ public class Player : MonoBehaviour
         else
         {
             rigid.velocity = new Vector2(rigid.velocity.x * inertia, rigid.velocity.y);
-        }
-    }
-
-    private void HandleGrappling()
-    {
-        Vector2 direction = (grapplePoint - rigid.position).normalized;
-        float distance = Vector2.Distance(rigid.position, grapplePoint);
-        lineRenderer.SetPosition(0, rigid.position);
-        lineRenderer.SetPosition(1, grapplePoint);
-
-        rigid.velocity = direction * grapplingSpeed;
-        Collider2D gpGround = Physics2D.OverlapBox(rigid.position, 
-        new Vector2(transform.localScale.x + 0.05f, transform.localScale.y * 2 + 0.05f), 0, LayerMask.GetMask("Ground"));
-        if (gpGround != null && (Vector2.Distance(rigid.position, grapplePoint) < Vector2.Distance(rigid.position, grappleStartPoint) || Vector2.Distance(grapplePoint, grappleStartPoint) < 1.5f))
-        {
-            RangeAttack(50);
-            lineRenderer.SetPosition(0, Vector2.zero);
-            lineRenderer.SetPosition(1, Vector2.zero);
-            isGrappling = false;
-            rigid.velocity = Vector2.zero;
         }
     }
 
@@ -160,22 +124,9 @@ public class Player : MonoBehaviour
         }
     }
 
-    void OnStrongAttack() // Grappling Hook
+    void OnStrongAttack()
     {
-        if (weapon == 3)
-        {
-            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-            RaycastHit2D hit = Physics2D.Raycast(rigid.position, mousePosition - rigid.position, 7f, LayerMask.GetMask("Ground"));
-
-            if (hit.collider != null)
-            {
-                grapplePoint = hit.point;
-                grappleStartPoint = rigid.position;
-                lineRenderer.SetPosition(0, rigid.position);
-                lineRenderer.SetPosition(1, grapplePoint);
-                isGrappling = true;
-            }
-        }
+        
     }
 
     private void RangeAttack(float damage)
