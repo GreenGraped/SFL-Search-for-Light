2e9fb703951a8879b9760973a92fef8349c80321
GreenGraped <121028592+GreenGraped@users.noreply.github.com>
2024-08-27 16:24:26 +0900

    a

--- Assets/Code/Boss/Mephistopeles.cs
+++ Assets/Code/Boss/Mephistopeles.cs
@@ -9,6 +9,7 @@ public class Mephistopeles : MonoBehaviour
     private Player playerSc;
 
     private Vector2 idlePosition = new Vector2(123, 5);
+    private bool wait = false;
 
     void Start() {
         player = GameManager.Instance.player;
@@ -17,13 +18,62 @@ public class Mephistopeles : MonoBehaviour
 
     public void BossStart()
     {
-        transform.position = new Vector2(123, 12);
-        MoveToIdle();
+        Appear();
+        //if (!wait)
+        //{
+        //    MoveToIdle();
+        //}
+        //if (!wait)
+        //{
+        //    StartCoroutine(Idle(3f));
+        //}
+        //if (!wait)
+        //{
+        //    Debug.Log("asdf");
+        //}
+        // 함수 끝나고 다음 함수 실행?
+        
     }
 
+    private void Appear()
+    {
+        transform.position = new Vector2(123, 12);
+        StartCoroutine(AppearAnim());
 
+    }
     private void MoveToIdle() {
+        wait = true;
         StartCoroutine(MoveToIdleCo());
+    }
+
+    IEnumerator Idle(float t)
+    {
+        wait = true;
+        yield return new WaitForSeconds(t);
+        wait = false;
+    }
+
+    IEnumerator AppearAnim()
+    {
+        wait = true;
+        for (int i = 0; i < 60; i++)
+        {
+            transform.position -= new Vector3(0, 0.05f);
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.7f);
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 40; j++)
+            {
+                transform.position -= new Vector3(0, 0.05f);
+                yield return null;
+            }
+            yield return new WaitForSeconds(0.7f);
+        }
+        wait = false;
+        transform.position = idlePosition;
     }
 
     IEnumerator MoveToIdleCo() {
@@ -35,7 +85,8 @@ public class Mephistopeles : MonoBehaviour
                 t = Mathf.Clamp01(elapsedTime / 1f); // 0.7f is duration
                 transform.position = Vector3.Lerp(startPosition, idlePosition, t);
                 yield return null;
-        }
+            }
+        wait = false;
     }
 
     private void Dash() {
